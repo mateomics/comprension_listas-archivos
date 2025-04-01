37a6ba436abd6b6014a82943d18ee44c98efc40f
Mateo Jimenez <majiso@lcg.unam.mx>
2025-04-01 14:52:31 -0600

    Agregué los directorios bin, data, doc, results y src, y guardé el primer ejercicio en bin

---
+++ bin/len_sequences.py
@@ -0,0 +1,5 @@
+print('¡Hola! Este código pide una secuencia de DNA y regresa la cantidad de bases de la misma.')
+secuencia = input('Introduzca una secuencia de DNA, compuesta sólo de las bases "A", "T", "C" o "G": ')
+
+print(f'La secuencia introducida es: {secuencia}')
+print(f'La longitud de la secuencia es de {len(secuencia)} bases.')
